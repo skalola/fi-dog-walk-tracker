ed5896feadf6169beef45739843f322433cd5365
skalola <shivkalola@gmail.com>
2019-09-11 11:25:15 -0400

    Initial Commit

---
+++ Fi_dog_walk_trackerTests/Fi_dog_walk_trackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Fi_dog_walk_trackerTests.swift
+//  Fi_dog_walk_trackerTests
+//
+//  Created by Shiv Kalola on 9/11/19.
+//  Copyright © 2019 Shiv Kalola. All rights reserved.
+//
+
+import XCTest
+@testable import Fi_dog_walk_tracker
+
+class Fi_dog_walk_trackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
